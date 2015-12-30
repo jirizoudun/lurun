7ee3087ea5b2d85e93e10cb0e7ed11829ad0b3bb
Tomas Fedor <tomas.fedor3@gmail.com>
2015-12-30 03:48:36 +0100

    Update tests

--- test.rb
+++ test.rb
@@ -6,14 +6,34 @@ cwd = File.expand_path(File.dirname(__FILE__))
 tests  = cwd+"/tests/"
 binary = cwd+"/bin/lurun.exe"
 
-Dir.entries(tests).select do |filename|
-  file = tests + filename
+
+def cleanup(filename)
+  clfile = filename.chomp(".lua") + ".cleanup"
+  return unless File.file? clfile
+  File.open(clfile).each do |line|
+    next if /[\/\\]/.match line
+    
+    del = File.expand_path(line.strip)
+    next unless File.file? del
+    
+    print "deleting "+del+"\n"
+    File.delete del 
+  end
+end
+
+Dir.chdir(tests)
+
+Dir.entries(".").select do |filename|
+  file = filename
 
   next unless File.file? file
   next unless /^test_.+\.lua$/.match filename
 
   sol = `lua #{file}`
+  cleanup filename
+
   res = `#{binary} #{file}c`
+  cleanup filename
 
   diff = Diffy::Diff.new(sol, res)
   success = diff.to_s == ""
